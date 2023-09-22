167d82d2bf74d5e80078ef2652eef23ce4f23d69
Samstiles-Dev <ayosamuel777@gmail.com>
2023-09-22 13:12:57 -0700

    function to check

--- atoi.c
+++ atoi.c
@@ -1,113 +1,94 @@
 #include "shell.h"
-#include <stddef.h>
 
 /**
- * parse - splits a string into words
+ * **parse - splits a string into words
  * @str: the input string
- * @delimiters: the delimiter string
+ * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
-char **parse(const char *str, const char *delimiters)
+
+char **parse(char *str, char *d)
 {
-int i, j, k, m, numwords = 0;
-char **tokens;
+	int i, j, k, m, numwords = 0;
+	char **s;
 
-if (str == NULL || str[0] == '\0')
-return (NULL);
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!_delimeter(str[i], d) && (_delimeter(str[i + 1], d) || !str[i + 1]))
+			numwords++;
 
-if (!delimiters)
-delimiters = " ";
-
-for (i = 0; str[i] != '\0'; i++) {
-if (!strchr(delimiters, str[i])) {
-while (str[i] && !strchr(delimiters, str[i]))
-                i++;
-            numwords++;
-        }
-    }
-
-if (numwords == 0)
-return NULL;
-
-tokens = malloc((numwords + 1) * sizeof(char *));
-if (!tokens)
-return NULL;
-
-for (i = 0, j = 0; j < numwords; j++) {
-while (str[i] && strchr(delimiters, str[i]))
-            i++;
-
-        k = 0;
-while (str[i + k] && !strchr(delimiters, str[i + k]))
-            k++;
-
-        tokens[j] = malloc((k + 1) * sizeof(char));
-        if (!tokens[j]) {
-            for (k = 0; k < j; k++)
-                free(tokens[k]);
-            free(tokens);
-            return NULL;
-        }
-
-        for (m = 0; m < k; m++)
-            tokens[j][m] = str[i++];
-        tokens[j][m] = '\0';
-    }
-
-    tokens[j] = NULL;
-    return tokens;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (_delimeter(str[i], d))
+			i++;
+		k = 0;
+		while (!_delimeter(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
 }
 
 /**
- * strtow2 - splits a string into words using a single delimiter character
+ * **strtow2 - splits a string into words
  * @str: the input string
- * @delimiter: the delimiter character
+ * @d: the delimeter
  * Return: a pointer to an array of strings, or NULL on failure
  */
-char **strtow2(const char *str, char delimiter)
+char **strtow2(char *str, char d)
 {
-    int i, j, k, m, numwords = 0;
-    char **tokens;
+	int i, j, k, m, numwords = 0;
+	char **s;
 
-    if (str == NULL || str[0] == '\0')
-        return NULL;
-
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] != delimiter) {
-            while (str[i] && str[i] != delimiter)
-                i++;
-            numwords++;
-        }
-    }
-
-    if (numwords == 0)
-        return NULL;
-
-    tokens = malloc((numwords + 1) * sizeof(char *));
-    if (!tokens)
-        return NULL;
-
-    for (i = 0, j = 0; j < numwords; j++) {
-        while (str[i] == delimiter)
-            i++;
-
-        k = 0;
-        while (str[i + k] != delimiter && str[i + k])
-            k++;
-
-        tokens[j] = malloc((k + 1) * sizeof(char));
-        if (!tokens[j]) {
-            for (k = 0; k < j; k++)
-                free(tokens[k]);
-            free(tokens);
-            return NULL;
-        }
-
-        for (m = 0; m < k; m++)
-            tokens[j][m] = str[i++];
-        tokens[j][m] = '\0';
-    }
-
-    tokens[j] = NULL;
-    return tokens;
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
 }
---
+++ checks.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * active - Checks whether the shell is running in interactive mode
+ * @info: A pointer to a struct containing shell information
+ *
+ * Return: 1 if the shell is in interactive mode, 0 otherwise
+ */
+int active(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * _delimeter - Checks if a character is a delimiter
+ * @c: The character to be checked
+ * @delim: The delimiter string to compare against
+ * Return: 1 if the character is a delimiter, 0 if not
+ */
+int _delimeter(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+		{
+			return (1);
+		}
+	return (0);
+}
+
+/**
+ * _alpha_char - Checks if a character is alphabetic
+ * @c: The character to be tested
+ * Return: 1 if 'c' is an alphabetic character, 0 otherwise
+ */
+int _alpha_char(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The input string to be converted
+ * Return: The converted integer, or 0 if no numbers are found in the string
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
